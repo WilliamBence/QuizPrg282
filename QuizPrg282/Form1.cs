8326bff398dddd71cc0537c231f81d38fde90e86
WilliamBence <91613279+WilliamBence@users.noreply.github.com>
2021-10-04 15:35:33 +0200

    Update Form1.cs
    Added reading Functionality from github

--- QuizPrg282/Form1.cs
+++ QuizPrg282/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace QuizPrg282
 {
@@ -15,6 +16,17 @@ namespace QuizPrg282
         public Form1()
         {
             InitializeComponent();
+        }
+        
+        public void ReadTheFile()
+        {
+            string[] lines = File.ReadAllLines("File.txt");
+            listBox1.Items.AddRange(lines);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReadTheFile();
         }
     }
 }
