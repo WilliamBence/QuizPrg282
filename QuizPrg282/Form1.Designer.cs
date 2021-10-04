6bc14f6a5c6b6e58dd6b0e7921352c415bb91fdf
WilliamBence <578143@student.belgiumcampus.ac.za>
2021-10-04 15:05:46 +0200

    Added listbox, button and text file

--- QuizPrg282/Form1.Designer.cs
+++ QuizPrg282/Form1.Designer.cs
@@ -29,13 +29,34 @@ namespace QuizPrg282
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(39, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(522, 147);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Read File";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(706, 266);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,6 +64,9 @@ namespace QuizPrg282
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
